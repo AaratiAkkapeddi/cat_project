1f71c00fd5b99ac927745bccdc728a1d19e3ad0a
Aarati Akkapeddi <aarati.akkapeddi@gmail.com>
2024-11-06 17:19:33 -0500

    changes path to image

--- sketch.js
+++ sketch.js
@@ -1,15 +1,22 @@
+let bg;
+
+function preload(){
+  bg = loadImage("./background.jpeg");
+}
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
   rectMode(CENTER)
+  angleMode(DEGREES)
 }
 
 function draw() {
+ image(bg, 0, 0, width, height)
+  // background(255)
  drawCat();
+  
 }
-
-
-
-
 
 
 
@@ -25,9 +32,13 @@ function drawCat(){
   rect(width/2,height/2 + 90,100,120,50,50,10,10);
   //tail
   push()
-  translate(width/2 + 60,height/2 + 90)
-  rotate(-45)
-  rect(0,0,100,10,100);
+  translate(width/2 + 30,height/2 + 100)
+  rotate(map(mouseY,0, height,-45, -30));
+  rect(50,0,100,15,100);
+  fill(255)
+  rect(100,0,10,1,100);
+  rect(100,2,10,1,100);
+  rect(100,4,10,1,100);
   pop()
   //ears
   triangle(width/2 - 50, height/2 - 50, width/2 - 50, height/2 - 20, width/2 - 30, height/2 - 30)
